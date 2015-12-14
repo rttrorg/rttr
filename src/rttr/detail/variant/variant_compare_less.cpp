0c550dd4e117fc8e77db6c8b529e6d1b1adcfc2d
Axel Menzel <info@axelmenzel.de>
2015-12-14 19:37:29 +0100

    - adjusted the email and the year in the copy right notice   from 'info@axelmenzel.de' to 'info@rttr.org'

--- src/rttr/detail/variant/variant_compare_less.cpp
+++ src/rttr/detail/variant/variant_compare_less.cpp
@@ -1,6 +1,6 @@
 /************************************************************************************
 *                                                                                   *
-*   Copyright (c) 2014, 2015 Axel Menzel <info@axelmenzel.de>                       *
+*   Copyright (c) 2014, 2015 - 2016 Axel Menzel <info@rttr.org>                     *
 *                                                                                   *
 *   This file is part of RTTR (Run Time Type Reflection)                            *
 *   License: MIT License                                                            *
