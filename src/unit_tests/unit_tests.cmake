2cc5af0ba9094f897152a908cf09daacbd7c6acd
Axel Menzel <info@axelmenzel.de>
2016-01-17 14:34:13 +0100

    added unit test for int32_t conversion

--- src/unit_tests/unit_tests.cmake
+++ src/unit_tests/unit_tests.cmake
@@ -71,5 +71,6 @@ set(SOURCE_FILES main.cpp
                  variant/variant_conv_to_bool.cpp
                  variant/variant_conv_to_int8.cpp
                  variant/variant_conv_to_int16.cpp
+                 variant/variant_conv_to_int32.cpp
                  variant_array_view/variant_array_view_test.cpp
                  )
