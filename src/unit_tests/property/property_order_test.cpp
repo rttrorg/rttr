f09688e7b147648105526934e12b7eed3bb42a57
Dan Dees <dand@appsmiths.com>
2018-09-17 12:59:45 -0500

    remove 2nd RTTR_ENABLE_OVERRIDE

--- src/unit_tests/property/property_order_test.cpp
+++ src/unit_tests/property/property_order_test.cpp
@@ -44,15 +44,6 @@ struct property_order_test_base
 
 };
 
-// RTTR_ENABLE incompatible with clang
-#define RTTR_ENABLE_OVERRIDE(...) \
-public:\
-    RTTR_INLINE rttr::type get_type() const override { return rttr::detail::get_type_from_instance(this); }  \
-    RTTR_INLINE void* get_ptr() override { return reinterpret_cast<void*>(this); } \
-    RTTR_INLINE rttr::detail::derived_info get_derived_info() override { return {reinterpret_cast<void*>(this), rttr::detail::get_type_from_instance(this)}; } \
-    using base_class_list = TYPE_LIST(__VA_ARGS__);
-
-
 struct property_order_test_derived : property_order_test_base
 {
     property_order_test_derived()
@@ -60,9 +51,10 @@ struct property_order_test_derived : property_order_test_base
     {}
     ~property_order_test_derived() override = default ;
 
+    // member with SAME name as base
     std::string _whoami ;
 
-    RTTR_ENABLE_OVERRIDE(property_order_test_base)
+    RTTR_ENABLE(property_order_test_base)
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
