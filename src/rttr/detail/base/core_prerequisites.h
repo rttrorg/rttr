fe98842255defe9f0b922f46b2dd1c067088a1f2
Andreas Schwab <schwab@suse.de>
2019-04-02 09:34:47 +0200

    Support all LP64 architectures

--- src/rttr/detail/base/core_prerequisites.h
+++ src/rttr/detail/base/core_prerequisites.h
@@ -89,7 +89,7 @@ namespace rttr
 // Architecture
 /////////////////////////////////////////////////////////////////////////////////////////
 #if defined(__x86_64__) || defined(_M_X64) || defined(__powerpc64__) || defined(__alpha__) ||\
-    defined(__ia64__) || defined(__s390__) || defined(__s390x__)
+    defined(__ia64__) || defined(__s390__) || defined(__s390x__) || defined(_LP64) || defined(__LP64__)
 #   define RTTR_ARCH_TYPE RTTR_ARCH_64
 #else
 #   define RTTR_ARCH_TYPE RTTR_ARCH_32
