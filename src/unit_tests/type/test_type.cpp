2f0289e086c0d89b53de5749fea49c0ad4ef6b87
Ken Noland <ken.noland@fundamentalgames.com>
2022-06-01 20:11:04 +0200

    fix for the unit tests for the type metadata

--- src/unit_tests/type/test_type.cpp
+++ src/unit_tests/type/test_type.cpp
@@ -636,14 +636,14 @@ TEST_CASE("Test rttr::type - get_metadata()", "[type]")
     t.foreach_metadata([&metadata](const rttr::detail::metadata& meta) { metadata.push_back(meta); });
 
     CHECK(metadata.size() == 4);
-    CHECK(metadata[0].get_key() == "Test");
-    CHECK(metadata[0].get_value() == "foo");
-    CHECK(metadata[1].get_key() == "other_key");
-    CHECK(metadata[1].get_value() == "bar");
-    CHECK(metadata[2].get_key() == "bar");
-    CHECK(metadata[2].get_value() == 42);
-    CHECK(metadata[3].get_key() == "foobar");
-    CHECK(metadata[3].get_value() == "hello");
+    CHECK(strcmp(metadata[0].get_key().convert<const char*>(), "Test") == 0);
+    CHECK(metadata[0].get_value().to_string() == "foo");
+    CHECK(metadata[1].get_key().to_string() == "bar");
+    CHECK(metadata[1].get_value().to_int() == 42);
+    CHECK(metadata[2].get_key().to_string() == "foobar");
+    CHECK(metadata[2].get_value().to_string() == "hello");
+    CHECK(metadata[3].get_key().to_string() == "other_key");
+    CHECK(metadata[3].get_value().to_string() == "bar");
 
     // negative
     CHECK(t.get_metadata("novalid key").is_valid() == false);
